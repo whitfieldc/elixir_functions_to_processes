473c6206a4f644f683789493d3374236621328b6
Charles <whitfield.cw@gmail.com>
2015-09-29 22:40:46 -0700

    add through multiple response types

--- named_function.exs
+++ named_function.exs
@@ -1,5 +1,44 @@
+# processified
+# defmodule NamedFunction do
+#   def call do
+#     receive do
+#       {_pid, msg} ->
+#         IO.puts "called with message #{msg}"
+#     end
+#   end
+# end
+
+# send message back to calling process
+# defmodule NamedFunction do
+#   def call do
+#     receive do
+#       {pid, msg} ->
+#         send pid, {:ok, "called with message #{msg}"}
+#     end
+#   end
+# end
+
+#recursive
+# defmodule NamedFunction do
+#   def call do
+#     receive do
+#       {pid, msg} ->
+#         send pid, {:ok, "called with message #{msg}"}
+#         call
+#     end
+#   end
+# end
+
+#different responses to different messages
 defmodule NamedFunction do
-  def call(msg) do
-    IO.puts "called with message #{msg}"
+  def call do
+    receive do
+      {:pop, pid, msg} ->
+        send pid, {:ok, "POP: called with message #{msg}"}
+        call
+      {:push, pid, msg} ->
+      send pid, {:ok, "PUSH: called with message #{msg}"}
+      call
+    end
   end
 end
